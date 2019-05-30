7bbd544b13c4a1446e71b5f64170d2b2dfb2e2ce
HHALPTEKİN <alptekin_-_@hotmail.com>
2019-05-30 15:01:16 +0300

    Add project files.

---
+++ OrganikBesinlerMVC/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrganikBesinlerMVC.Models.Entity;
+
+namespace OrganikBesinlerMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        OrganikBesinlerEntities db = new OrganikBesinlerEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            var degerler = db.OrganikÜrünler.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ OrganikBesinlerMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OrganikBesinlerMVC.Startup))]
+namespace OrganikBesinlerMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
